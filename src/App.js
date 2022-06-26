9397359f9096df9097e358f3ef1fa60759f7e653
Amy Sandoval <amysandoval92@gmail.com>
2022-06-26 15:05:40 -0700

    Add props, states, and callback handlers

--- src/App.js
+++ src/App.js
@@ -1,68 +1,65 @@
 import * as React from "react";
+import List from "./List";
 
 const welcome = {
   greeting: "Hey",
   title: "React",
 };
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = React.useState("");
 
-function List() {
-  return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}{" "}
-    </ul>
-  );
-}
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
 
-function Search() {
+    props.onSearch(event);
+  };
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
     </div>
   );
-}
+};
 
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>
         {welcome.greeting} {welcome.title}
       </h1>
-      <Search />
+      <Search onSearch={handleSearch} />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
   );
-}
+};
 
 export default App;
