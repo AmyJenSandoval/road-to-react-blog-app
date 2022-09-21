148fac500abdaaf741112594f2b24bdd902bb588
Amy Sandoval <amysandoval92@gmail.com>
2022-09-21 14:40:04 -0700

    Remove unused variable

--- src/List.js
+++ src/List.js
@@ -8,29 +8,24 @@ const List = ({ list, onRemoveItem }) => (
   </ul>
 );
 
-const Item = ({ item, onRemoveItem }) => {
-  const handleRemoveItem = () => {
-    onRemoveItem(item);
-  };
-  return (
-    <li className="item" key={item.objectID}>
-      <span style={{ width: "40%" }}>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span style={{ width: "30%" }}>{item.author}</span>
-      <span style={{ width: "10%" }}>{item.num_comments}</span>
-      <span sytle={{ width: "10%" }}>{item.points}</span>
-      <span style={{ width: "10%" }}>
-        <button
-          type="button"
-          onClick={() => onRemoveItem(item)}
-          className="button button_small"
-        >
-          Dismiss
-        </button>
-      </span>
-    </li>
-  );
-};
+const Item = ({ item, onRemoveItem }) => (
+  <li className="item" key={item.objectID}>
+    <span style={{ width: "40%" }}>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span style={{ width: "30%" }}>{item.author}</span>
+    <span style={{ width: "10%" }}>{item.num_comments}</span>
+    <span sytle={{ width: "10%" }}>{item.points}</span>
+    <span style={{ width: "10%" }}>
+      <button
+        type="button"
+        onClick={() => onRemoveItem(item)}
+        className="button button_small"
+      >
+        Dismiss
+      </button>
+    </span>
+  </li>
+);
 
 export default List;
